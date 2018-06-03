8e15431b1107ebb2c32bd7aecb91565eee562826
nitesh <mrniteshdiwan@gmail.com>
2018-06-03 15:02:16 +0530

    bubble sorting

---
+++ sorting/bubblesort.java
@@ -0,0 +1,18 @@
+class bubblesort{
+	public static void main(String[] args){
+		int[] a={5,6,4,7,3,2};
+		int n=a.length;
+		for(int i=0;i<n-1;i++){
+			for(int j=0;j<n-i-1;j++){
+				if(a[j]>a[j+1]){
+					int temp=a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;
+				}
+			}
+		}
+		for(int i=0;i<n;i++){
+			System.out.print(a[i]+" ");
+		}
+	}
+}
